342e95455e270fd8cf2dd1b71c0f71c9234e2490
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-01-10 16:53:56 -0500

    SpringBoot template project

---
+++ Tapestry/src/main/java/com/revature/Tapestry/TapestryApplication.java
@@ -0,0 +1,12 @@
+package com.revature.Tapestry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapestryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapestryApplication.class, args);
+	}
+}
