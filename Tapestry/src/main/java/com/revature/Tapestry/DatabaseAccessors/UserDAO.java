aa80a512c98233974563138937637359b4d3367b
Cooper Cummings <coopercummings6@gmail.com>
2018-01-12 19:53:50 -0500

    fixed wrong types in UserDAO

--- Tapestry/src/main/java/com/revature/Tapestry/DatabaseAccessors/UserDAO.java
+++ Tapestry/src/main/java/com/revature/Tapestry/DatabaseAccessors/UserDAO.java
@@ -3,9 +3,9 @@ package com.revature.Tapestry.DatabaseAccessors;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.revature.Tapestry.beans.Post;
+import com.revature.Tapestry.beans.User;
 
 @Repository
-public interface UserDAO extends JpaRepository<Post,Integer>{
+public interface UserDAO extends JpaRepository<User,Integer>{
 
 }
