8acde7bc08149d42d55dc1a626d67906f7bf5b7a
Cooper Cummings <coopercummings6@gmail.com>
2018-01-14 19:30:47 -0500

    I doubt I will make progress tonight, I can't figure out how to get an instance of a DAO, so maybe someone else will see what I don't

--- Tapestry/src/test/java/com/revature/Tapestry/TapestryApplicationTests.java
+++ Tapestry/src/test/java/com/revature/Tapestry/TapestryApplicationTests.java
@@ -1,16 +1,46 @@
 package com.revature.Tapestry;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.PersistenceContext;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.revature.Tapestry.DatabaseAccessors.BoardDAO;
+import com.revature.Tapestry.beans.Board;
+import com.revature.Tapestry.beans.Post;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceContext.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class})
 public class TapestryApplicationTests {
+	
+	//@Autowired
+    //BoardDAO boardDAO;
 
 	@Test
-	public void contextLoads() {
+	public void basicDatabaseConnectivityExists() {
+		//List<Post> postList = new ArrayList<Post>();
+		//Board testBoard = new Board("testBoard", postList);
+		//ArrayList<Board> boards = new ArrayList<Board>();
+		//boards.add(testBoard);
+		//boardDAO.save(boards);
+		//List<Board> searchresults = boardDAO.findAll();
+		//assertTrue(searchresults.size() != 0);
+		assertTrue(true);//temp to test something
 	}
 
 }
