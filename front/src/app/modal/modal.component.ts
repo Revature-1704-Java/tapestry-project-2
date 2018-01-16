57895b2403e4a98e79f906544c8362a472061bdb
Kelsey Yuen <heavenslashinghell@gmail.com>
2018-01-16 15:33:52 -0500

    Added ability to create dummy thread and replies

--- front/src/app/modal/modal.component.ts
+++ front/src/app/modal/modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup } from '@angular/forms/src/model';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { TokenService } from '../shared/token.service';
+import { CurrentViewService } from '../shared/current-view.service';
 
 @Component({
   selector: 'app-modal',
@@ -11,11 +12,14 @@ import { TokenService } from '../shared/token.service';
 export class ModalComponent implements OnInit {
   private user = { email: '', password: '' };
   private newuser = { username: '', email: '', password: '' };
+  private newpost = { title: '', body: ''}
+
   private myForm: FormGroup = this.myForm;
   @ViewChild('loginClose') loginClose: ElementRef;
   @ViewChild('signupClose') signupClose: ElementRef;
+  @ViewChild('postClose') postClose: ElementRef;
 
-  constructor(private httpClient: HttpClient, public token: TokenService) { }
+  constructor(private httpClient: HttpClient, public token: TokenService, public curView: CurrentViewService) { }
 
   ngOnInit(): void {
   }
@@ -67,5 +71,28 @@ export class ModalComponent implements OnInit {
           this.signupClose.nativeElement.click();
         }
       }, err => console.log(err));
+  }
+
+  createPost(): void {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+    const body = new HttpParams()
+      .set('userId', this.token.id.toString())
+      .set('body', this.newpost.body);
+    if (this.curView.view == 'catalog') {
+      body.set('title', this.newpost.title);
+    } else {
+      body.set('title', '');
+    }
+
+    const header = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded');
+
+    this.httpClient.post(apiUrl, body, { headers: header })
+      .subscribe(res => {
+        console.log(res);
+        this.postClose.nativeElement.click();
+      }, err => console.log(err));
+
   }
 }
--- front/src/app/shared/thread.ts
+++ front/src/app/shared/thread.ts
@@ -1,6 +1,7 @@
 export interface Thread {
     // picture: string;
+    userId: number;
+    id: number;
     title: string;
     body: string;
-    id: number;
 }
