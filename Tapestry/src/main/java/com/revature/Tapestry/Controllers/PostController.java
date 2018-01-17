9f29af09fde55d87266e7bcb8b5369e664f7f755
Cooper Cummings <coopercummings6@gmail.com>
2018-01-17 14:03:32 -0500

    added code to add images (as inputstreams) into s3 when I get the rest of the needed information. Keys will be email/photoname (email will have @ replaced with .)

--- Tapestry/src/main/java/com/revature/Tapestry/Controllers/PostController.java
+++ Tapestry/src/main/java/com/revature/Tapestry/Controllers/PostController.java
@@ -1,15 +1,12 @@
 package com.revature.Tapestry.Controllers;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
@@ -17,13 +14,12 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.revature.Tapestry.DatabaseAccessors.BoardDAO;
 import com.revature.Tapestry.DatabaseAccessors.PostDAO;
 import com.revature.Tapestry.DatabaseAccessors.UserDAO;
-import com.revature.Tapestry.beans.Board;
-import com.revature.Tapestry.beans.Comment;
 import com.revature.Tapestry.beans.Post;
-import com.revature.Tapestry.beans.User;
 
 @RestController
 public class PostController {
@@ -58,7 +54,7 @@ public class PostController {
 		return postDao.findAll().stream().collect(Collectors.toList());
 	}	
 	
-	@PostMapping(value="/createThread", consumes=MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value="/createThread")
 	public void submitPost()
 	{
 		AWSCredentialsProvider credentials = new AWSStaticCredentialsProvider 
@@ -74,7 +70,24 @@ public class PostController {
 		//Now doesn't work. SQL error Integrity violated.
 	}*/
 	
-	@PostMapping(value="/createReply/{id}", consumes=MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value="createPost")
+	public void submitComment()
+	{
+		//get s3client
+		String bucketName = "bucketOfPhotos";
+		AWSCredentialsProvider credentials = new AWSStaticCredentialsProvider 
+				(new BasicAWSCredentials(System.getenv("ACCESSKEY"), System.getenv("SECRETKEY")));
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(credentials)
+                .build();
+		
+		//code to insert an image to s3
+		InputStream image = null;
+		String key = "";
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, image, new ObjectMetadata());
+		s3Client.putObject(putObjectRequest);
+	}
+	/*@PostMapping(value="/createReply/{id}", consumes=MediaType.APPLICATION_JSON_VALUE)
 	public void createReply(@RequestBody Comment comment, @PathVariable("id") int id) {
 		Post p = postDao.findOne(id);
 		List<Comment> replies = p.getReplies();
@@ -82,6 +95,6 @@ public class PostController {
 		p.setReplies(replies);
 		postDao.save(p);
 		//Getting an 500 internal server error "object references an unsaved transient instance"
-	}
+	}*/
 	
 }
