7acb61a6714c7a1795fd13cfe1b5d087e078381e
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-01-18 12:38:00 -0500

    Create Board endpoint

--- Tapestry/src/main/java/com/revature/Tapestry/Controllers/BoardController.java
+++ Tapestry/src/main/java/com/revature/Tapestry/Controllers/BoardController.java
@@ -1,9 +1,17 @@
 package com.revature.Tapestry.Controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.Tapestry.DatabaseAccessors.BoardDAO;
 import com.revature.Tapestry.DatabaseAccessors.PostDAO;
+import com.revature.Tapestry.beans.Board;
+import com.revature.Tapestry.beans.Post;
+import com.revature.Tapestry.beans.User;
 
 @RestController
 public class BoardController {
@@ -14,6 +22,19 @@ public class BoardController {
 		this.boardDao = boardDao;
 		this.postDao = postDao;
 	}
+	
+	//Get a thread. Post and all replies
+		@PostMapping(value="/createBoard")
+		public ResponseEntity<?> getPosts(@RequestParam("postId") int boardId, @RequestParam("boardName") String boardName) {
+			Board board = boardDao.findOne(boardId);
+			if (board ==  null){
+				board = new Board(boardName);
+				boardDao.save(board);
+				return new ResponseEntity<>(HttpStatus.CREATED);
+			}
+			
+			return new ResponseEntity<>("Board Name: " + board.getBoardName() + " already exists", HttpStatus.IM_USED);
+		}
 	
 	/*
 	 * Collection<Post> threads = postDao.findAll().stream().collect(Collectors.toList());
