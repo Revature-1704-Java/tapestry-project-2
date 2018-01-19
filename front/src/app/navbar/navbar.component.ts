1e33ff473af6dbc923be82a11f614076a8d8d401
Kelsey Yuen <heavenslashinghell@gmail.com>
2018-01-19 09:22:40 -0500

    Attempted fix of navbar reload

--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { CurrentViewService } from '../shared/current-view.service';
 
 @Component({
@@ -8,13 +10,17 @@ import { CurrentViewService } from '../shared/current-view.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public curView: CurrentViewService) { }
+  constructor(public curView: CurrentViewService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   switchBoard(newBoard: string): void {
     this.curView.board = newBoard;
+    
+    const curUrl = this.router.url;
+    this.router.navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate([curUrl]));
   }
 
 }
